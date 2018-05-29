42fe019e9f19221917073a351c574a55b9735291
Dylan Slogrove <dslogrov@student.wethinkcode.co.za>
2018-05-29 14:48:35 +0200

    fixed a norm error with swapnfree

--- ft_swapnfree.c
+++ ft_swapnfree.c
@@ -6,14 +6,14 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 13:32:13 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/05/29 13:34:46 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/05/29 14:48:22 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_swapnfree(char **var, char *new_val)
 {
 	char	*temp;
-	
+
 	temp = new_val;
 	free(*var);
 	*var = temp;
