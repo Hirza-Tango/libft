51c3db030638b0e33626f87c4ae1dfc185da1134
Dylan Slogrove <dslogrov@student.wethinkcode.co.za>
2018-05-31 14:59:26 +0200

    Fixed strneq

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/22 15:12:17 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/05/31 14:34:08 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/05/31 14:56:21 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,15 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 
 	if (!s1 || !s2)
 		return (0);
+	if (!n)
+		return (1);
 	c1 = (char *)s1;
 	c2 = (char *)s2;
 	while (*c1 && *c2 && n--)
 		if (*c1++ - *c2++)
+			return (0);
+	if (n + 1)
+		if (*c1 || *c2)
 			return (0);
 	return (1);
 }
