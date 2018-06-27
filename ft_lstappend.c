27674d4d900e2e105b34d19452afa539d5ff778e
Dylan Slogrove <dslogrove@gmail.com>
2018-06-27 16:29:45 +0200

    Added condition in ft_lstappend for empty list

--- ft_lstappend.c
+++ ft_lstappend.c
@@ -6,17 +6,21 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/27 15:23:15 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/06/27 15:28:36 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/06/27 16:16:55 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 void	ft_lstappend(t_list **alst, t_list *new)
 {
 	t_list	*current;
 
-	current = *alst;
+	if (!(current = *alst))
+	{
+		*alst = new;
+		return ;
+	}
 	while (current->next)
 		current = current->next;
 	current->next = new;
