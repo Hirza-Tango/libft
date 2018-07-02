32de79ba5c14b8ebca8f0f5242692f2f97ff528a
Dylan Slogrove <dslogrove@gmail.com>
2018-07-02 16:12:18 +0200

    Fixed bad include

--- ft_lstappend.c
+++ ft_lstappend.c
@@ -6,12 +6,12 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/27 15:23:15 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/06/27 16:16:55 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/07/02 13:10:09 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 void	ft_lstappend(t_list **alst, t_list *new)
 {
 	t_list	*current;
