9b5291b9b4aeb6e1a4d014f5145cae6e10b26d68
Dylan Slogrove <dslogrove@gmail.com>
2018-08-15 13:50:10 +0200

    Added actual ft_lstrm file this time

---
+++ ft_lstrm.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstrm.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/15 13:38:44 by dslogrov          #+#    #+#             */
+/*   Updated: 2018/08/15 13:49:41 by dslogrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	ft_lstrm(t_list **list, size_t index, void (*del)(void *, size_t))
+{
+	size_t	i;
+	t_list	*dup;
+	t_list	*next;
+
+	if (!list || !*list)
+		return ;
+	if (!index)
+	{
+		next = (*list)->next;
+		ft_lstdelone(*list, del);
+		*list = next;
+		return ;
+	}
+	dup = *list;
+	i = 0;
+	while (dup->next)
+	{
+		if (i == index - 1)
+		{
+			next = dup->next->next;
+			ft_lstdelone(&(dup->next), del);
+			dup->next = next;
+			return ;
+		}
+		i++;
+		dup = dup->next;
+	}
+}
