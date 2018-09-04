618e0075f4d69ca9b6ddb65583532e99d56c4d4e
Dylan Slogrove <dslogrove@gmail.com>
2018-09-04 13:25:29 +0200

    Added NULL protection in ft_lstdel

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 10:24:06 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/05/23 12:09:21 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/09/04 13:25:02 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 	t_list	*curr;
 	t_list	*next;
 
+	if (!alst)
+		return ;
 	curr = *alst;
 	while (curr)
 	{
