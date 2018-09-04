b768cc358a43b1ed60a42eb9548ea20aecebe4a8
Dylan Slogrove <dslogrove@gmail.com>
2018-09-04 14:19:50 +0200

    Added safer code for t_d_list

--- ft_dlstdel.c
+++ ft_dlstdel.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 11:07:52 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/08/28 11:14:44 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/09/04 13:48:06 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	ft_dlstdel(t_d_list **alst, void (*del)(void*, size_t))
 {
-	ft_lstdel((t_list **)alst, del);
+	t_d_list	*curr;
+	t_d_list	*next;
+
+	if (!alst)
+		return ;
+	curr = *alst;
+	while (curr)
+	{
+		next = curr->next;
+		ft_dlstdelone(&curr, del);
+		curr = next;
+	}
+	*alst = NULL;
 }
--- ft_dlstdelone.c
+++ ft_dlstdelone.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 11:05:13 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/09/03 17:40:36 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/09/04 13:40:48 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_dlstdelone(t_d_list **alst, void (*del)(void*, size_t))
 {
-	ft_lstdelone((t_list **)alst, del);
+	del((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = NULL;
 }
