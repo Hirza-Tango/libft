4adaeb6df3e97a1b78d4bd630a01956912d36b8c
Dylan Slogrove <dslogrove@gmail.com>
2019-06-27 16:24:41 +0200

    Added '0x' to putptr

--- ft_putptr_fd.c
+++ ft_putptr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 16:10:27 by dslogrov          #+#    #+#             */
-/*   Updated: 2019/06/27 16:17:50 by dslogrov         ###   ########.fr       */
+/*   Updated: 2019/06/27 16:23:06 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,24 @@
 
 void	ft_putptr_fd(void *ptr, int fd)
 {
-	uintmax_t n;
+	uintmax_t	n;
+	static char init;
 
+	if (!init)
+		ft_putstr_fd("0x", fd);
+	init = 1;
 	n = (uintmax_t)ptr;
-	if (n < 10)
-		ft_putchar_fd(n + '0', fd);
-	else if (n < 16)
-		ft_putchar_fd(n - 10 + 'A', fd);
+	if (n < 16)
+	{
+		if (n < 10)
+			ft_putchar_fd(n + '0', fd);
+		else
+			ft_putchar_fd(n - 10 + 'A', fd);
+		init = 0;
+	}
 	else
 	{
-		ft_putnbr_fd(n / 16, fd);
+		ft_putptr_fd((void *)(n / 16), fd);
 		if (n % 16 < 10)
 			ft_putchar_fd(n % 16 + '0', fd);
 		else
